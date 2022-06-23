f59b26311af98862a49732a754f173a8160646b0
L.E. Nichols <le_nichols@yahoo.com>
2022-06-22 19:51:51 -0700

    storybook changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      util: require.resolve('util/'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
